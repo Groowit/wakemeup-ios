260be030ee448ae8aea5390d78c74a4c58eba569
yeayoungkim <yeayoungkim.22@gmail.com>
2026-04-16 00:01:33 +0900

    Initial Commit

---
+++ wakemeup/wakemeupApp.swift
@@ -0,0 +1,17 @@
+//
+//  wakemeupApp.swift
+//  wakemeup
+//
+//  Created by 김예영 on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct wakemeupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ wakemeupTests/wakemeupTests.swift
@@ -0,0 +1,17 @@
+//
+//  wakemeupTests.swift
+//  wakemeupTests
+//
+//  Created by 김예영 on 4/16/26.
+//
+
+import Testing
+@testable import wakemeup
+
+struct wakemeupTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
